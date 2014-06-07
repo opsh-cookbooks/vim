50dc4b4b6492c474f336ce83e3752ee64ff1e552
ops-dev-sh <ops-dev-sh@users.noreply.github.com>
2014-06-08 02:28:48 +0800

    init with vundle

---
+++ recipes/default.sh
@@ -0,0 +1,13 @@
+#
+
+apt-install vim git
+
+echo "[notice] using vundle"
+
+[ -f ~/.vim/bundle ] || mkdir ~/.vim/bundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+templte "vimrc" ~/.vimrc
+
+vi /tmp/1 +:PluginInstall
+
