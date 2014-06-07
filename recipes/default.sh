dbbb5d725efdfc3220e17b46031294cb568a60c0
ops-dev-sh <ops-dev-sh@users.noreply.github.com>
2014-06-08 02:45:45 +0800

    fix error

--- recipes/default.sh
+++ recipes/default.sh
@@ -5,9 +5,9 @@ apt-install vim git
 echo "[notice] using vundle"
 
 [ -f ~/.vim/bundle ] || mkdir ~/.vim/bundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
 
 template "vimrc" ~/.vimrc
 
-vi /tmp/1 +:PluginInstall
+vi /tmp/1 +:PluginInstall +:q! +:q!
 
